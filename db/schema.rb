27460fe2ddb13bc6a48f028fc7931312eff1caf1
Mauricio <mauriciofb@gmail.com>
2023-08-05 00:26:39 -0400

    Agrega esqueletos de template para generador Scaffold <3

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_203738) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_011930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "supports", force: :cascade do |t|
+    t.date "date"
+    t.string "company"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
