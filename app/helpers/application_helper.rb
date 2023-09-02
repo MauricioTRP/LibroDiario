27aed51e0be9804d5d03c27abf115aa4890ae4ac
Mauricio <mauriciofb@gmail.com>
2023-09-02 02:44:02 -0400

    Next iteration on dinamic select helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,25 +2,25 @@ module ApplicationHelper
   def region_address(form)
     form.select(
       :region_id,
-      Region.all.collect{ |t| [t.region, t.id] },
-      { include_blank: true},
-      { class: 'h-10 border mt-1 rounded px-4 w-full bg-gray-50' }
-    )
-  end
-
-  def province_address(form)
-    form.select(
-      :province_id,
-      Province.all.collect{ |t| [t.provincia, t.id] },
+      Region.all.collect { |t| [t.region, t.id] },
       { include_blank: true },
       { class: 'h-10 border mt-1 rounded px-4 w-full bg-gray-50' }
     )
   end
 
-  def comune_address(form)
+  def province_address(form, region_selection = 13)
+    form.select(
+      :province_id,
+      Province.where("region_id = #{region_selection}").collect { |t| [t.provincia, t.id] },
+      { include_blank: true },
+      { class: 'h-10 border mt-1 rounded px-4 w-full bg-gray-50' }
+    )
+  end
+
+  def comune_address(form, province_selection = 39)
     form.select(
       :comune_id,
-      Comune.all.collect{ |t| [t.comuna, t.id] },
+      Comune.where("province_id = #{province_selection}").collect { |t| [t.comuna, t.id] },
       { include_blank: true },
       { class: 'h-10 border mt-1 rounded px-4 w-full bg-gray-50' }
     )
